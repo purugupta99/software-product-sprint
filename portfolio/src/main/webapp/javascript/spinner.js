236f0cd87694ab70fab2882a1af59d94213eeea3
Puru Gupta <guptapuru304@gmail.com>
2020-07-14 01:18:06 +0530

    Initial Commit

---
+++ portfolio/src/main/webapp/javascript/spinner.js
@@ -0,0 +1,7 @@
+setTimeout(function(){
+   document.getElementById('loading').style.display = 'none';
+}, 5000);
+
+setTimeout(function(){
+   document.getElementById('enter-button').style.display = 'initial';
+}, 5000);
